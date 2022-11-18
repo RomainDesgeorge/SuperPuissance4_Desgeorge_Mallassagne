5d1213531a89aeadebde5c799c7d381591e1d71c
romain desgeorge <romain.desgeorge@epfedu.fr>
2022-11-18 15:02:08 +0100

    Création de la méthode présenceJeton()

--- src/sp4_mallassagne_desgeorge/CelluleDeGrille.java
+++ src/sp4_mallassagne_desgeorge/CelluleDeGrille.java
@@ -9,5 +9,23 @@ package sp4_mallassagne_desgeorge;
  * @author rom
  */
 public class CelluleDeGrille {
+    public CelluleDeGrille(){
+        jetonCourant = null;
+        avoirTrouNoir = false;
+        avoirDesintegrateur = false;
+
+    
+}
+    private Jeton jetonCourant;
+    private boolean avoirTrouNoir;
+    private boolean avoirDesintegrateur;
+    
+    public boolean presenceJeton(){
+        boolean verif = false;
+        if(jetonCourant != null){
+            verif = true;            
+        }
+        return verif;
+    }
     
 }
