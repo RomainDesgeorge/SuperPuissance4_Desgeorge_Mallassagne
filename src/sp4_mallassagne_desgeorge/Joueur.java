4ca2f485f9b70a046066895085b1f3d642992485
Alexis <alexis.mallassagne@epfedu.fr>
2022-11-18 16:16:04 +0100

    Initialisation classe joueur

--- src/sp4_mallassagne_desgeorge/Joueur.java
+++ src/sp4_mallassagne_desgeorge/Joueur.java
@@ -5,14 +5,33 @@
 package sp4_mallassagne_desgeorge;
 import java.util.Scanner;
 import java.util.ArrayList;
+
 /**
  *
  * @author Alexis
  */
 public class Joueur {
     private String nom;
-    public Joueur (String Nom, String Couleur, ArrayList reserveJetons, int nombreJetonsRestants, int nombreDesintegrateurs){
-        nom=Nom;
-        nombreJetonsRestants=reserveJetons.size();
+    ArrayList <Jeton> reserveJetons=new ArrayList<>();
+    String couleur;
+    int nombreJetonsRestants;
+    int nombreDesintegrateurs=0;
+    //nombreJetonsRestants=reserveJetons.size();
+    public Joueur (String Nom){
+        nom=Nom;       
+    }
+    public String affecterCouleur(String Couleur){
+        return Couleur;
+    }
+    public void ajouterJeton(Jeton ajoutjeton){
+        nombreJetonsRestants +=1;
+        reserveJetons.add(ajoutjeton);
+    }
+    public void obtenirDesintegrateur(){
+        nombreDesintegrateurs+=1;
+    }
+    public void utiliserDesintegrateur(){
+        nombreDesintegrateurs-=1;
+        System.out.println("Le désintegrateur a bien été utilisé");
     }
 }
