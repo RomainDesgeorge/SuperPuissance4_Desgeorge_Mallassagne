d4857851e284aa4fdb7ffb282e928d3a16ded982
romain desgeorge <romain.desgeorge@epfedu.fr>
2022-11-18 17:13:12 +0100

    En train de créer la fonction ligneGagnantePourCouleur(String)

--- src/sp4_mallassagne_desgeorge/PlateauDeJeu.java
+++ src/sp4_mallassagne_desgeorge/PlateauDeJeu.java
@@ -42,6 +42,36 @@ public class PlateauDeJeu {
         }
         return verif;
     }
+    public boolean presenceJeton(int x, int y){
+        boolean verif = false;
+        if (grille[x][y].presenceJeton() == true){
+            verif = true;
+        }
+        return verif;
+    }
+    public String lireCouleurDuJeton(int x, int y){
+        String couleur;
+        couleur = grille[x][y].lireCouleurDuJeton();
+        return couleur;
+    }
+    public boolean ligneGagnantePourCouleur(String couleur){
+        boolean verif = false;
+        int win = 0;
+        int verif1 = 0;
+        for (int i = 0;i<6;i++){
+            for(int j = 0; j<7;j++){
+                if (verif1 == 0){
+                    if (grille[i][j].lireCouleurDuJeton()== couleur){
+                        win+=1;
+                    }else{
+                        verif1 = 1;
+                    }
+
+                }
+            }
+        }
+        return verif;
+    }
 }
 
 
