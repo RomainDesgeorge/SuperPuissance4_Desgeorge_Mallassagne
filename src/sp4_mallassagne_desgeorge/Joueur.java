d7eeb5818b7689edd73a3b6c087c5ff9d45104e5
Alexis <alexis.mallassagne@epfedu.fr>
2022-11-28 16:15:55 +0100

    Modification joueur

--- src/sp4_mallassagne_desgeorge/Joueur.java
+++ src/sp4_mallassagne_desgeorge/Joueur.java
@@ -8,30 +8,85 @@ import java.util.ArrayList;
 
 /**
  *
- * @author Alexis
+ * @author Alexis Mallassagne et Romain Desgeorgesssss
  */
 public class Joueur {
     private String nom;
-    ArrayList <Jeton> reserveJetons=new ArrayList<>();
-    String couleur;
-    int nombreJetonsRestants;
+    private ArrayList <Jeton> reserveJetons=new ArrayList<>();
+    private String couleur;
+    int nombreJetonsRestants=reserveJetons.size();
     int nombreDesintegrateurs=0;
-    //nombreJetonsRestants=reserveJetons.size();
+    
+    /**
+     *Constructeur Joueur récupère le Nom du joueur
+     * @param Nom
+     */
     public Joueur (String Nom){
-        nom=Nom;       
+        nom=Nom;     
+        nombreDesintegrateurs=0;
     }
-    public String affecterCouleur(String Couleur){
-        return Couleur;
+
+    /**
+     *Prend la couleur du joueur et la retourne
+     * @param Couleur
+     * @return
+     */
+    public void affecterCouleur(String Couleur){
+        Couleur=Couleur;
     }
+
+    /**
+     *recupère la resèrve de jetons
+     * @return
+     */
+    public ArrayList<Jeton> getReserveJetons() {
+        return reserveJetons;
+    }
+    public Jeton jouerJeton(){
+        Jeton jeton;
+        jeton=reserveJetons.get(0);
+        reserveJetons.remove(0) ;
+        return jeton;
+    }
+    /**
+     *Prend un jeton en entrée et l'ajoute dans le Tableau reserveJetons
+     * @param ajoutjeton
+     */
     public void ajouterJeton(Jeton ajoutjeton){
-        nombreJetonsRestants +=1;
         reserveJetons.add(ajoutjeton);
+        
+
     }
+
+    /**
+     *Recupère la couleur
+     * @return
+     */
+    public String getCouleur() {
+        return couleur;
+    }
+
+
+    /**
+     *Retourne le nombre de jetons restants (ça actualise la valeur)
+     * @return
+     */
+    public int TailleTabJeton(){
+        return nombreJetonsRestants=reserveJetons.size();
+    }
+
+    /**
+     *Ajoute 1 au nombre de Desintegrateurs
+     */
     public void obtenirDesintegrateur(){
         nombreDesintegrateurs+=1;
     }
+
+    /**
+     *Soustrait 1 au nombre de Desintegrateurs
+     */
     public void utiliserDesintegrateur(){
         nombreDesintegrateurs-=1;
-        System.out.println("Le désintegrateur a bien été utilisé");
+        System.out.println("Le désintegrateur a bien été utilisé.");
     }
 }
