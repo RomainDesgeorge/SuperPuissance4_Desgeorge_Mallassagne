227375d84ed3a5bf77368595f1cc48ce938e3cbc
Alexis <alexis.mallassagne@epfedu.fr>
2022-12-02 15:29:02 +0100

    Choix des noms des joueurs

--- src/sp4_mallassagne_desgeorge/SP4_Mallassagne_Desgeorge.java
+++ src/sp4_mallassagne_desgeorge/SP4_Mallassagne_Desgeorge.java
@@ -4,6 +4,7 @@
  */
 package sp4_mallassagne_desgeorge;
 
+import java.util.Scanner;
 
 /**
  *
@@ -15,12 +16,20 @@ public class SP4_Mallassagne_Desgeorge {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Joueur joueur1 = new Joueur("joueur1");
-        Joueur joueur2 = new Joueur("joueur2");
-        Partie partie = new Partie(joueur1,joueur2);
-        partie.initialiserPartie();
-        partie.lancerPartie(); 
-        
+        String Nom_joueur1;
+        String Nom_joueur2;
+        Scanner Nom1 = new Scanner(System.in);
+        Scanner Nom2 = new Scanner(System.in);
+        System.out.println("Joueur 1, Rentrez votre nom");
+        Nom_joueur1 = Nom1.next();
+        System.out.println("Joueur 2, Rentrez votre nom");
+        Nom_joueur2 = Nom2.next();
+        Joueur joueur1 = new Joueur(Nom_joueur1);//création des 2 joueurs
+        Joueur joueur2 = new Joueur(Nom_joueur2);
+        Partie partie = new Partie(joueur1, joueur2);//création d'une nouvelle partie prenant en paramètres les 2 joueurs
+        partie.initialiserPartie();//initialisation de la partie
+        partie.lancerPartie(); //lancement de la partie
+
     }
-    
+
 }
